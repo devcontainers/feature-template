602e1b01cd8a49348e44743ad19d272d3376ea25
Tom Irgang <thomas@irgang.eu>
2024-12-17 11:23:24 +0000

    fix build tools installation

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Image Development
 
 Dev container features for embedded Linux image development.
+
+## Locally test the features
+
+- `devcontainer features test -f imagedev -i debian:latest`
+- `devcontainer features test -f imagedev -i ubuntu:latest`
--- test/imagedev/test.sh
+++ test/imagedev/test.sh
@@ -11,6 +11,11 @@ check "check python3" bash -c "python3 --version"
 check "check git" bash -c "git --version"
 check "check sudo" bash -c "sudo echo 'Hello from root'"
 
+check "check build" bash -c "ls -lah /build"
+check "check venv" bash -c "ls -lah /build/venv"
+check "check tools" bash -c "ls -lah /build/tools"
+check "check build tools" bash -c "test -d /build/tools/build_tools"
+
 # Report results
 # If any of the checks above exited with a non-zero exit code, the test will fail.
 reportResults
